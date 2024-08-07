f45bd89f8f4cf5184acf9ebc004c4942a0ba7551
safarov-en <safarov.elkhan110@gmail.com>
2024-08-07 13:34:22 +0500

    Error component

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import Header from "home/Header";
 import Footer from "home/Footer";
+import SafeComponent from "./SafeComponent";
 
 const App = () => (
   <div className=" text-3xl mx-auto max-w-6xl">
-    <Header />
+    <SafeComponent>
+      <Header />
+    </SafeComponent>
     <div className="my-10">Контент главной страницы</div>
     <Footer />
   </div>
