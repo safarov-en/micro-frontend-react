963f9b64faab4999c4b73df2ed2624e0f014932a
safarov-en <safarov.elkhan110@gmail.com>
2024-08-08 14:09:29 +0500

    Adding routes

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import "./index.scss";
 import Header from "home/Header";
 import Footer from "home/Footer";
-import SafeComponent from "./SafeComponent";
 import PDPContent from "./PDPContent";
 
-const App = () => (
+const App = ({children}) => (
   <div className=" text-3xl mx-auto max-w-6xl">
-    <SafeComponent>
-      <Header />
-    </SafeComponent>
+    <Header />
     <div className="my-10">
-      <PDPContent />
+      {children}
     </div>
     <Footer />
   </div>
 );
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>
+  },
+  {
+    path: '/product/:id',
+    element: <App><PDPContent /></App>
+  }
+])
 const rootElement = document.getElementById("app")
 if (!rootElement) throw new Error("Failed to find the root element")
 
 const root = ReactDOM.createRoot(rootElement)
 
-root.render(<App />)
+root.render(<RouterProvider router={router} />)
